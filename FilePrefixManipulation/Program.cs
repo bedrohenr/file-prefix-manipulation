ae339ad0772e2063b1478f7fcc56c7e3f6b017d8
Pedro <ddrppx@gmail.com>
2024-09-17 21:34:31 -0300

    searching directories first

--- FilePrefixManipulation/Program.cs
+++ FilePrefixManipulation/Program.cs
@@ -3,25 +3,27 @@ using System.IO.Enumeration;
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
+    private static void Main(string[] args) {
         Console.WriteLine("Started.");
 
         string InsertedPath = "D:\\bkp\\Music\\MEmu Music";
         string[] FilePaths = Directory.GetFiles(InsertedPath);
         string[] DirPaths = Directory.GetDirectories(InsertedPath);
 
-        Console.WriteLine("Files: ");
-        foreach(string FileName in FilePaths) {
-            Console.WriteLine(FileName);
-        }
-        Console.WriteLine("End of files.");
-        Console.WriteLine("");
         Console.WriteLine("Directories: ");
         foreach (string DirName in DirPaths)
         {
             Console.WriteLine(DirName);
         }
         Console.WriteLine("End of directories.");
+
+        Console.WriteLine("");
+
+        Console.WriteLine("Files: ");
+        foreach(string FileName in FilePaths) {
+            Console.WriteLine(FileName);
+        }
+        Console.WriteLine("End of files.");
+        
     }
 }
