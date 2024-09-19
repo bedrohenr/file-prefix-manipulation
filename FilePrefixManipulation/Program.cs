c721d4dd5d5d7d37efda8b4cc4807cf64e363564
Pedro <ddrppx@gmail.com>
2024-09-18 21:48:45 -0300

    fixing variable name

--- FilePrefixManipulation/Program.cs
+++ FilePrefixManipulation/Program.cs
@@ -6,11 +6,13 @@ internal class Program
     private static void Main(string[] args) {
         Console.WriteLine("Started.");
 
-        string prefix = "[SPOTIFY-DOWNLOADER.COM] ";
-        string InsertedPath = "D:\\bkp\\Music\\MEmu Music";
+        // Strings
+        string Prefix, InsertedPath, FileName, NewFileName, NewFilePath;
+
+        Prefix = "[SPOTIFY-DOWNLOADER.COM] ";
+        InsertedPath = "D:\\bkp\\Music\\MEmu Music";
         string[] FilePaths = Directory.GetFiles(InsertedPath);
         string[] DirPaths = Directory.GetDirectories(InsertedPath);
-        string? FileName, NewFileName, NewFilePath;
 
         // TODO
         // Make recurrent calls inside every folder?
@@ -27,8 +29,8 @@ internal class Program
         Console.WriteLine("Files: ");
         foreach(string FilePath in FilePaths) {
             FileName = Path.GetFileName(FilePath);
-            if(FileName.Contains(prefix)){
-                NewFileName = FileName.Replace(prefix,"");
+            if(FileName.Contains(Prefix)){
+                NewFileName = FileName.Replace(Prefix,"");
                 NewFilePath = Path.GetDirectoryName(FilePath) + @"\" + NewFileName;
                 File.Move(FilePath, NewFilePath);
                 Console.WriteLine($"Renamed {FileName} to {NewFileName}.");
